b5cfcde3c7085d0a6e25e3c4fe2910d8a2051c80
manorom <manorom@users.noreply.github.com>
2023-03-12 14:31:33 +0100

    Make `Slurm` object construction cleaner

--- wormulon/slurm.py
+++ wormulon/slurm.py
@@ -17,22 +17,22 @@ class SlurmAcct:
         else:
             return None
 
-    def all(self):
-        return [Job.from_queue(e) for e in self.session.sacct()]
+    def all(self, user_id=None):
+        return [Job.from_queue(e) for e in self.session.sacct(user_id)]
 
 
 class SlurmQueue:
     def __init__(self, session):
         self.session = session
 
-    def submit(self, jobspec, wait=False, **kwargs):
+    def submit(self, jobspec, wait=False):
         if type(jobspec) is str:
             jobexec = jobspec
             jobargs = {}
         else:
             jobexec = jobspec.exec
             jobargs = jobspec.construct_args()
-        return self.session.sbatch(jobexec, **jobargs)
+        return self.session.sbatch(jobexec, wait=wait, **jobargs)
 
     def lookup(self, job):
         if type(job) is int:
@@ -52,26 +52,23 @@ class SlurmQueue:
 
 # TODO: integrate user_id (required for squeue and sacct)
 class Slurm:
-    def __init__(
-            self, session=None, host=None, username=None, password=None, local=False
-    ):
-        if session:
-            self.session = session
-        elif host:
-            sshargs = {}
-            if username:
-                sshargs["username"] = username
-            if password:
-                sshargs["password"] = password
-            self.session = SSHSession(host, **sshargs)
-        elif local:
-            self.session = LocalSession()
-        else:
-            raise ValueError(
-                "Slurm.__init__ needs to be called with argument session or ssh_host set, or local set to True"
-            )
+    def __init__(self, session):
+        self.session = session
         self.queue = SlurmQueue(self.session)
         self.acct = SlurmAcct(self.session)
+
+    @classmethod
+    def ssh(cls, host, username=None, password=None):
+        sshargs = {}
+        if username:
+            sshargs["username"] = username
+        if password:
+            sshargs["password"] = password
+        return cls(SSHSession(host, **sshargs))
+
+    @classmethod
+    def local(cls):
+        return cls(LocalSession())
 
     def submit(self, jobspec, wait=False, **kwargs):
         job_id = self.queue.submit(jobspec, wait=wait, **kwargs)
